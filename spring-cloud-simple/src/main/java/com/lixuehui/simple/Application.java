4890d83abbe5b04423bbddcc2fcb49c2c62fd259
xuehui <lxhzmy@126.com>
2019-04-03 09:59:26 +0800

    add logback

--- spring-cloud-simple/src/main/java/com/lixuehui/simple/Application.java
+++ spring-cloud-simple/src/main/java/com/lixuehui/simple/Application.java
@@ -1,5 +1,7 @@
 package com.lixuehui.simple;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,9 +10,11 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.lixuehui.simple"})
 public class Application {
+    private final static Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        logger.info("Simple Application started.");
     }
 
 }
